2c3580e832edf6860b7a7511b9f6513d4ab81ac9
Burak Orkmez <burakorkmezz@gmail.com>
2024-02-28 16:44:02 +0300

    cron.js added

--- backend/index.js
+++ backend/index.js
@@ -19,8 +19,12 @@ import mergedTypeDefs from "./typeDefs/index.js";
 import { connectDB } from "./db/connectDB.js";
 import { configurePassport } from "./passport/passport.config.js";
 
+import job from "./cron.js";
+
 dotenv.config();
 configurePassport();
+
+job.start();
 
 const __dirname = path.resolve();
 const app = express();
